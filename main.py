97cccce05b69500495487d1a4c09c7c73bb0bd4d
Stanislav Yanakiev <stan5050@gmail.com>
2023-04-05 10:31:25 +0300

    'methods, functions, select'

---
+++ main.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+# employees = ['Stan', 'John', 'Teddy']
+# age = [38, 39, 1]
+#
+# data = {
+#     'Names': employees,
+#     'Age': age,
+#     }
+#
+# my_data_frame = pd.DataFrame(data)
+# print(my_data_frame)
+
